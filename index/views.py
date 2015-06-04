860d7ea1968fe03ac05a5e6f512290a15dc77349
turb <turb@pisscake.com>
2015-06-03 19:56:35 -0700

    moved it into objects

--- index/views.py
+++ index/views.py
@@ -9,24 +9,50 @@ import datetime
 # Create your views here.
 def index(request):
 	if request.method == 'POST':
-		username = request.POST.get("username", "")
-		if not username.isalnum():
-			return HttpResponse('wow, nice username, to bad it\'s not alphanumeric')
+		try:
+			handleUsercredentials(request)
+		except:
+			return HttpResponse("you did something wrong (probably)")
 
-		hash_salt = hashlib.md5(str(time.time()).encode()).hexdigest()
-		hashed_password = hashlib.sha512(request.POST.get("password", "").encode()).hexdigest()
+	latest_jack = jack.objects.order_by('date')[0]
+	template = loader.get_template('index/index.html')
+	context = {'latest_jack': latest_jack}
+	return render(request, 'index/index.html', context)
 
-		newUser = user(
-			name=username,
-			password_hash=hashed_password,
-			last_online=datetime.datetime.today(),
-			creation_date=datetime.datetime.today(),
-			password_salt=hash_salt)
-		newUser.save()
-		return HttpResponse("all is well")
-		#return HttpResponse("yep" + str(request.POST.get("password", "")) + ";")
-	else:
-		latest_jack = jack.objects.order_by('date')[0]
-		template = loader.get_template('index/index.html')
-		context = {'latest_jack': latest_jack}
-		return render(request, 'index/index.html', context)
+def handleUsercredentials(request):
+	username = str(request.POST.get('username', ''))
+	password = str(request.POST.get('password', ''))
+	action = str(request.POST.get('action', ''))
+
+	if username == '':
+		raise Exception('most provide a username')
+	if password == '':
+		raise Exception('most provide a password')
+
+	if action == 'signup':
+		signup(username, password)
+	elif action == 'signin':
+		pass
+
+def signin(username, password):
+	pass
+
+def signup(username, password):
+	if not username.isalnum():
+		raise Exception('username must be alphanumeric')
+	if userExists(username):
+		raise Exception('username already exists')
+
+	hash_salt = hashlib.md5(str(time.time()).encode()).hexdigest()
+	hashed_password = hashlib.sha512(password.encode()).hexdigest()
+
+	newUser = user(
+		name=username,
+		password_hash=hashed_password,
+		last_online=datetime.datetime.today(),
+		creation_date=datetime.datetime.today(),
+		password_salt=hash_salt)
+	newUser.save()
+
+def userExists(username):
+	return False;
