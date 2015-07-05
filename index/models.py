4937021bb29d062d1eb7d8d848fa6ab93568c23d
turb <turb@pisscake.com>
2015-07-05 02:53:06 -0700

    time for the backend

--- index/models.py
+++ index/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class user(models.Model):
 	name = models.CharField(max_length=16)
 	password_hash = models.CharField(max_length=128)
@@ -12,6 +11,23 @@ class jack(models.Model):
 	user_id = models.ForeignKey(user)
 	date = models.DateTimeField()
 	comment = models.CharField(max_length=160)
+
+class geolocation(models.Model):
+	jack = models.ForeignKey(jack)
+	lat = models.DecimalField(max_digits=11, decimal_places=8)
+	lng = models.DecimalField(max_digits=11, decimal_places=8)
+
+class link(models.Model):
+	jack = models.ForeignKey(jack)
+	url = models.CharField(max_length=2083)
+
+class image(models.Model):
+	jack = models.ForeignKey(jack)
+	path = models.CharField(max_length=2083)
+
+class jack_bro(models.Model):
+	jack = models.ForeignKey(jack)
+	bro = models.ForeignKey(user)
 
 class yes_word(models.Model):
 	word = models.CharField(max_length=32)
