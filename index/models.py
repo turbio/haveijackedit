6a37f9d44318eccdf5fb4016b5404dc93b62c503
turb <turb@pisscake.com>
2015-07-08 21:21:23 -0700

    image model has a source field

--- index/models.py
+++ index/models.py
@@ -33,8 +33,17 @@ class link(models.Model):
 
 class image(models.Model):
 	jack = models.ForeignKey('jack')
-	data = models.ImageField(upload_to='./data/jack_image/')
+	#data = models.ImageField(upload_to='./data/jack_image/')
 	data = models.CharField(max_length=50000)
+
+	SOURCES = (
+		('f', 'file'),
+		('c', 'camera'))
+
+	source = models.CharField(
+		max_length='1',
+		choices=SOURCES,
+		default='f')
 
 class jack_bro(models.Model):
 	jack = models.ForeignKey('jack')
