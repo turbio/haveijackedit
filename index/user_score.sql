f2ce53e2632a2a2db8b814263489c8cce04f4867
turb <turb@pisscake.com>
2015-08-27 14:31:24 -0700

    user score query

---
+++ index/user_score.sql
@@ -0,0 +1,31 @@
+SELECT
+	(votes.total_recieved * 5) + jacks.unique_dates + FLOOR(jacks.total_submitted_length / jacks.total_submitted) AS score
+FROM
+	index_user
+LEFT JOIN (
+	SELECT
+		index_usersubmitted.user_id AS user_id,
+		SUM(index_vote.points) AS total_recieved
+	FROM
+		index_vote
+	INNER JOIN index_jack ON
+		index_jack.usersubmitted_ptr_id = index_vote.jack_id
+	INNER JOIN index_usersubmitted ON
+		index_usersubmitted.id = index_jack.usersubmitted_ptr_id
+	GROUP BY
+		index_usersubmitted.user_id
+) AS votes ON votes.user_id = index_user.id
+LEFT JOIN (
+	SELECT
+		index_usersubmitted.user_id AS user_id,
+		COUNT(index_jack.usersubmitted_ptr_id) AS total_submitted,
+		SUM(CHAR_LENGTH(index_jack.comment)) AS total_submitted_length,
+		COUNT(DISTINCT DATE(index_jack.date)) AS unique_dates
+	FROM
+		index_jack
+	INNER JOIN index_usersubmitted ON
+		index_usersubmitted.id = index_jack.usersubmitted_ptr_id
+	GROUP BY
+		index_usersubmitted.user_id
+) AS jacks ON jacks.user_id = index_user.id
+WHERE index_user.id = %s
